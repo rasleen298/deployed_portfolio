aef722d7500e8fb7f81616a7dee091b5d077ffa0
rasleen298 <rasleen0209@gmail.com>
2019-05-18 20:44:00 +0000

    jjj

---
+++ local_setings.py
@@ -0,0 +1,14 @@
+SECRET_KEY = 'dajwc*0d5@s!d)=gr40m@)##277o9)%e*iyde%g+ob+1f0@+123cr(c#6'
+
+ALLOWED_HOSTS = ['159.89.173.184']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'djangohost',
+        'PASSWORD':'djangohost@123',
+        'HOST': 'localhost',
+        'PORT':'5432',
+    }
+}
